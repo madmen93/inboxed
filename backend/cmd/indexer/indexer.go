e8101950e3a0229cfb6bba4eaa9bdc127a4c07a6
Monica Daza <158379665+madmen93@users.noreply.github.com>
2025-01-28 21:43:36 -0500

    eliminado código comentado en indexer

--- backend/cmd/indexer/indexer.go
+++ backend/cmd/indexer/indexer.go
@@ -33,27 +33,6 @@ func deleteIndex(index string, zincURL string) error {
 		return err
 	}
 
-	// indexURL := fmt.Sprintf("%s/api/index/%s", os.Getenv("ZINC_URL"), index)
-
-	// req, err := http.NewRequest("DELETE", indexURL, nil)
-	// if err != nil {
-	// 	return fmt.Errorf("error en el request DELETE: %v", err)
-	// }
-	// req.SetBasicAuth(os.Getenv("ZINC_USER"), os.Getenv("ZINC_PASSWORD"))
-
-	// client := &http.Client{}
-	// resp, err := client.Do(req)
-	// if err != nil {
-	// 	return fmt.Errorf("error en el retorno de la respuesta http: %v", err)
-	// }
-	// defer resp.Body.Close()
-
-	// if resp.StatusCode != http.StatusOK {
-	// 	return fmt.Errorf("error en la eliminación del index. StatusCode: %d", resp.StatusCode)
-	// }
-
-	// log.Println("Index deleted successfully")
-
 	return nil
 }
 
@@ -69,26 +48,6 @@ func createIndexZincSearch(indexer IndexerData, url string) error {
 	if err != nil {
 		return err
 	}
-
-	// indexURL := fmt.Sprintf("%s/api/index", url)
-
-	// req, err := http.NewRequest("POST", indexURL, bytes.NewBuffer(data))
-	// if err != nil {
-	// 	return fmt.Errorf("error en el método post en createIndexZincSearch: %v", err)
-	// }
-	// req.SetBasicAuth(os.Getenv("ZINC_USER"), os.Getenv("ZINC_PASSWORD"))
-	// req.Header.Set("Content-Type", "application/json")
-
-	// client := &http.Client{}
-	// resp, err := client.Do(req)
-	// if err != nil {
-	// 	return fmt.Errorf("error en el retorno de la respuesta http: %v", err)
-	// }
-	// defer resp.Body.Close()
-
-	// if resp.StatusCode != http.StatusOK {
-	// 	return fmt.Errorf("error al crear el indexer. StatusCode: %v", resp.StatusCode)
-	// }
 
 	return nil
 }
@@ -111,29 +70,5 @@ func sendToZincSearch(records []EmailData, zincURL string) error {
 		return err
 	}
 
-	// client := &http.Client{Timeout: 10 * time.Second}
-
-	// // bulkURL := fmt.Sprintf("%s/api/_bulkv2", zincURL)
-	// req, err := http.NewRequest("POST", bulkURL, bytes.NewReader(jsonData))
-	// if err != nil {
-	// 	return fmt.Errorf("error al crear solicitud POST: %v", err)
-	// }
-
-	// req.SetBasicAuth(os.Getenv("ZINC_USER"), os.Getenv("ZINC_PASSWORD"))
-	// req.Header.Set("Content-Type", "application/json")
-	// req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36")
-
-	// resp, err := client.Do(req)
-	// if err != nil {
-	// 	return fmt.Errorf("error al enviar datos a ZincSearch: %v", err)
-	// }
-	// defer resp.Body.Close()
-
-	// if resp.StatusCode != http.StatusOK {
-	// 	body, _ := io.ReadAll(resp.Body)
-	// 	return fmt.Errorf("error del servidor ZincSearch. StatusCode: %d, Body: %s", resp.StatusCode, string(body))
-	// }
-
-	// log.Println("Lote enviado correctamente a ZincSearch")
 	return nil
 }
